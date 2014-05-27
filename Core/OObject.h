9ac755cbec1302fb586334842b106e61fe1c2c76
ElectricCoffee <slench102+git@gmail.com>
2014-05-27 15:53:47 +0200

    Added basic commentary

--- Core/OObject.h
+++ Core/OObject.h
@@ -2,6 +2,10 @@
 
 @class String;
 
+/* it's called "OObject" so it doesn't interfere
+ * with the objc runtime's built-in Object class
+ * OObject is also fitting given the project's name.
+ */
 @interface OObject
 + (id) alloc;
 - (id) init;
--- Core/String.h
+++ Core/String.h
@@ -1,5 +1,11 @@
 #import "OObject.h"
 
+/* The premise of this project
+ * is to have all the built-in types be
+ * prefix-less, simply because it's much more
+ * natural to write String *str = @"Hello";
+ * than it is to write NSString *str = @"Hello";
+ */
 @interface String : OObject {
   const char* _val;
 }
